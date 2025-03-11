a474847546657c824208c5499d1b079872282b07
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2025-03-11 10:47:08 -0700

    feat: add detailed logging to TFIDF vectorization process
    Enhanced logging to provide better visibility into key steps, such as wiping directories, downloading files, and tokenizer updates. Included log messages for TEI-to-TXT conversion, curated entity retrieval, tokenizer operations, and vectorizer fitting. Configured logging output with timestamps for improved traceability.

--- agr_entity_extractor/agr_tfidf_vectorizer_helper.py
+++ agr_entity_extractor/agr_tfidf_vectorizer_helper.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 def fit_vectorizer_on_agr_corpus(mod_abbreviation: str = None, wipe_download_dir: bool = False):
     download_dir = os.getenv("AGR_CORPUS_DOWNLOAD_DIR", "/tmp/alliance_corpus")
     if wipe_download_dir:
+        logger.info(f"Wiping download directory: {download_dir}")
         if os.path.exists(download_dir):
             shutil.rmtree(download_dir)
             logger.info(f"{download_dir} has been wiped.")
@@ -27,15 +28,22 @@ def fit_vectorizer_on_agr_corpus(mod_abbreviation: str = None, wipe_download_dir
         txt_files_present = len([file for file in os.listdir(download_dir) if file.endswith(".txt")]) > 0
 
     if not tei_files_present and not txt_files_present:
+        logger.info("No TEI files found in the download directory. Downloading TEI files.")
+        logger.info(f"Getting all reference curies for {mod_abbreviation} from the Alliance ABC API.")
         ref_curies = get_all_ref_curies(mod_abbreviation=mod_abbreviation)
+        logger.info(f"Downloading TEI files for {len(ref_curies)} references.")
         download_tei_files_for_references(ref_curies, download_dir, mod_abbreviation=mod_abbreviation)
         tei_files_present = True
 
     if tei_files_present:
+        logger.info("TEI files found in the download directory. Converting TEI files to TXT files.")
         convert_all_tei_files_in_dir_to_txt(download_dir)
 
+    logger.info("Downloading list of curated genes and alleles from the Alliance ABC API and adding them to the "
+                "tokenizer.")
     curated_genes = get_all_curated_entities(mod_abbreviation=mod_abbreviation, entity_type_str="gene")
     curated_alleles = get_all_curated_entities(mod_abbreviation=mod_abbreviation, entity_type_str="allele")
+    logger.info("Loading BioBERT tokenizer and adding curated genes and alleles to it.")
     tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-base-cased-v1.2")
     tokenizer.add_tokens(curated_genes + curated_alleles, special_tokens=False)
 
@@ -46,10 +54,12 @@ def fit_vectorizer_on_agr_corpus(mod_abbreviation: str = None, wipe_download_dir
 
     tfidf_vectorizer = TfidfVectorizer(input='filename', tokenizer=custom_tokenizer)
     text_files = (os.path.join(download_dir, f) for f in os.listdir(download_dir) if f.endswith(".txt"))
+    logger.info(f"Fitting TFIDF vectorizer on text files.")
     tfidf_vectorizer.fit(text_files)
     string_vectorizer = TfidfVectorizer(vocabulary=tfidf_vectorizer.vocabulary_, tokenizer=custom_tokenizer)
     dummy_corpus = ["dummy document"]
     string_vectorizer.fit(dummy_corpus)
+    logger.info(f"TFIDF vectorizer fitted.")
     return string_vectorizer
 
 
@@ -83,6 +93,12 @@ def main():
     parser.add_argument("--wipe-download-dir", action="store_true",
                         help="If set, wipes the download directory before processing")
     args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        stream=None)
 
     vectorizer = fit_vectorizer_on_agr_corpus(mod_abbreviation=args.mod_abbreviation,
                                               wipe_download_dir=args.wipe_download_dir)
