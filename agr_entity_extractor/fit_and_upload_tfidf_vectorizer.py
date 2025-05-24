12bdd62eceb5305d8480ce89ed5f984325fef3ca
sweng66 <sweng@stanford.edu>
2025-05-24 07:22:33 -0700

    fixed to work with strain etc

--- agr_entity_extractor/fit_and_upload_tfidf_vectorizer.py
+++ agr_entity_extractor/fit_and_upload_tfidf_vectorizer.py
@@ -14,14 +14,20 @@ from utils.tei_utils import convert_all_tei_files_in_dir_to_txt
 logger = logging.getLogger(__name__)
 
 
-def fit_vectorizer_on_agr_corpus(mod_abbreviation: str = None, match_uppercase: bool = False,
-                                 wipe_download_dir: bool = False, continue_download: bool = False):
+def fit_vectorizer_on_agr_corpus(mod_abbreviation: str = None, topic: str = None,
+                                 match_uppercase: bool = False, wipe_download_dir: bool = False,
+                                 continue_download: bool = False):
     download_dir = os.getenv("AGR_CORPUS_DOWNLOAD_DIR", "/tmp/alliance_corpus")
     if wipe_download_dir and not continue_download:
         logger.info(f"Wiping download directory: {download_dir}")
         if os.path.exists(download_dir):
-            shutil.rmtree(download_dir)
-            logger.info(f"{download_dir} has been wiped.")
+            for entry in os.scandir(download_dir):
+                path = entry.path
+                if entry.is_dir():
+                    shutil.rmtree(path)
+                else:
+                    os.remove(path)
+        else:
             os.makedirs(download_dir, exist_ok=True)
 
     tei_files_present = False
@@ -49,12 +55,20 @@ def fit_vectorizer_on_agr_corpus(mod_abbreviation: str = None, match_uppercase: 
         logger.info("TEI files found in the download directory. Converting TEI files to TXT files.")
         convert_all_tei_files_in_dir_to_txt(download_dir)
 
-    logger.info("Downloading list of curated genes and alleles from the Alliance ABC API and adding them to the "
+    logger.info("Downloading list of curated entities from the Alliance ABC API and adding them to the "
                 "tokenizer.")
-    curated_genes, _ = get_all_curated_entities(mod_abbreviation=mod_abbreviation, entity_type_str="gene")
-    # curated_alleles, _ = get_all_curated_entities(mod_abbreviation=mod_abbreviation, entity_type_str="allele")
-    logger.info("Loading and add curated genes to it.")
-    custom_tokenizer = CustomTokenizer(tokens=curated_genes, match_uppercase_entities=match_uppercase)
+    curated_entities, _ = get_all_curated_entities(
+        mod_abbreviation=mod_abbreviation,
+        entity_type_str=(
+            "gene" if topic == "ATP:0000005"
+            else "allele" if topic == "ATP:0000006"
+            else "strain" if topic == "ATP:0000027"
+            else "gene"
+        )
+    )
+
+    logger.info("Loading and add curated entities to it.")
+    custom_tokenizer = CustomTokenizer(tokens=curated_entities, match_uppercase_entities=match_uppercase)
     tfidf_vectorizer = TfidfVectorizer(input='filename', tokenizer=lambda doc: custom_tokenizer.tokenize(doc))
     text_files = [os.path.join(download_dir, f) for f in os.listdir(download_dir) if f.endswith(".txt")]
     logger.info("Fitting TFIDF vectorizer on text files.")
@@ -93,6 +107,7 @@ def main():
         default="tfidf_vectorizer.pkl",
         help="Output file path to save the vectorizer (default: tfidf_vectorizer.pkl)."
     )
+    parser.add_argument("-t", "--topic", required=True, help="The topic of the model")
     parser.add_argument("--match-uppercase", action="store_true")
     parser.add_argument("--wipe-download-dir", action="store_true",
                         help="If set, wipes the download directory before processing")
@@ -115,6 +130,7 @@ def main():
     if not os.path.exists(args.output_path):
 
         vectorizer = fit_vectorizer_on_agr_corpus(mod_abbreviation=args.mod_abbreviation,
+                                                  topic=args.topic,
                                                   wipe_download_dir=args.wipe_download_dir,
                                                   continue_download=args.continue_download,
                                                   match_uppercase=args.match_uppercase)
@@ -122,8 +138,8 @@ def main():
         logger.info(f"TFIDF vectorizer saved to {args.output_path}.")
 
     if args.update_custom_tokenizer:
-        curated_genes, _ = get_all_curated_entities(mod_abbreviation=args.mod_abbreviation, entity_type_str="gene")
-        custom_tokenizer = CustomTokenizer(tokens=curated_genes)
+        curated_entities, _ = get_all_curated_entities(mod_abbreviation=args.mod_abbreviation, entity_type_str="gene")
+        custom_tokenizer = CustomTokenizer(tokens=curated_entities)
         vectorizer = load_vectorizer_from_file(args.output_path)
         vectorizer.tokenizer = lambda doc: custom_tokenizer.tokenize(doc)
         save_vectorizer_to_file(vectorizer, args.output_path)
@@ -136,7 +152,7 @@ def main():
             "average_f1": None,
             "best_params": None,
         }
-        upload_ml_model(task_type="tfidf_vectorization", mod_abbreviation=args.mod_abbreviation, topic=None,
+        upload_ml_model(task_type="tfidf_vectorization", mod_abbreviation=args.mod_abbreviation, topic=args.topic,
                         model_path=args.output_path, stats=stats, dataset_id=None, file_extension="dpkl")
         logger.info(f"TFIDF vectorizer uploaded to the Alliance API for {args.mod_abbreviation}.")
 
